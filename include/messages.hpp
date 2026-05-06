4cef2d021c84fd547ba76869daf746a0e198794a
usefull <the.usefull@gmail.com>
2026-05-06 17:45:51 +0300

    multithread + gracefull stop

--- include/messages.hpp
+++ include/messages.hpp
@@ -5,17 +5,20 @@
 
 using namespace std;
 
-namespace origo::ErrorMessages {
-
+namespace origo::ErrorMessages
+{
     inline constexpr string_view CantOpenConfigFile = "Unable to open configuration file {}";
     inline constexpr string_view ConfigFileReadingError = "Configuration file reading error: {}";
     inline constexpr string_view CantReadIpFromConfig = "Unable to read IP-address from configuration file  (field \"ip\"):  {}";
     inline constexpr string_view CantReadPortFromConfig = "Unable to read port number from configuration file  (field \"port\"):  {}";
 }
 
-namespace origo::InfoMessages {
-
+namespace origo::InfoMessages
+{
     inline constexpr string_view StartPrompt = "Origo({}) started on port {}";
+    inline constexpr string_view CtrlC = "Press Ctrl+C to stop gracefully...";
+    inline constexpr string_view StopSigRecivied = "Received stop signal. Shutting down gracefully...";
+    inline constexpr string_view StoppedSuccessfully = "Server stopped successfully.";
 }
 
 #endif // MESSAGES_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+#include <csignal>
+#include <atomic>
+#include <chrono>
+#include <thread>
+
 #include <restinio/all.hpp>
 #include <nlohmann/json.hpp>
 #include <fmt/core.h>
@@ -5,33 +10,39 @@
 #include "../include/config.hpp"
 #include "../include/messages.hpp"
 
-using namespace std;
-using json = nlohmann::json;
-namespace err = origo::ErrorMessages;
-namespace mess = origo::InfoMessages;
-
-int main(int argc, char* argv[])
-{
-    const auto config = origo::Config::from_file(argc > 1 ? argv[1] : "origo.conf");
-
-    using namespace restinio;
-
 #ifdef NDEBUG
     #define MODE "Release"
 #else
     #define MODE "Debug"
 #endif
 
-    cout << fmt::format(mess::StartPrompt, MODE, config.port) << endl;
+using namespace std;
+using json = nlohmann::json;
+namespace err = origo::ErrorMessages;
+namespace mess = origo::InfoMessages;
 
-    run(
-        on_this_thread()
-            .port(config.port)
+atomic<bool> stop_flag{false};
+
+int main(int argc, char* argv[])
+{
+    const auto config = origo::Config::from_file(argc > 1 ? argv[1] : "origo.conf");
+
+    using namespace restinio;
+    using traits_t = restinio::default_traits_t;
+
+    const auto cores = std::thread::hardware_concurrency();
+    const auto pool_size = std::max(cores, 2u);
+
+    restinio::http_server_t<traits_t> server
+    {
+        restinio::own_io_context(),
+        restinio::server_settings_t<traits_t>{}
             .address(config.ip)
+            .port(config.port)
             .request_handler([](auto req) {
                 json response;
                 response["message"] = "Hello from Origo!";
-                response["status"] = "ok";
+                response["status"] = "ok***";
                 response["path"] = req->header().path();
                 
                 return req->create_response()
@@ -39,7 +50,32 @@ int main(int argc, char* argv[])
                     .set_body(response.dump(4))
                     .done();
             })
-    );
+    };
+
+    restinio::on_pool_runner_t<restinio::http_server_t<traits_t>> runner
+    {
+        pool_size, 
+        server
+    };
+
+    signal(SIGINT, [](int) { stop_flag = true; });
+    signal(SIGTERM, [](int) { stop_flag = true; });
+
+    runner.start();
+
+    cout << fmt::format(mess::StartPrompt, MODE, config.port) << endl;
+    cout << mess::CtrlC << endl;
+    while(!stop_flag)
+    {
+        this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    cout << mess::StopSigRecivied << endl;
+    
+    runner.stop();
+    runner.wait();
+    
+    cout << mess::StoppedSuccessfully << endl;
     
     return 0;
 }
